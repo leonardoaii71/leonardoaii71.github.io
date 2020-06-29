95f3ae32095e02fdd1425c934fb94a451f8a6fa5
leonardo <leonardoaii71@gmail.com>
2020-06-29 10:04:56 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <dev-menu></dev-menu>
+  </div>
+</template>
+
+<script>
+import menu from "./components/dev-menu.vue";
+
+export default {
+  name: "App",
+  components: {
+    'dev-menu': menu,
+  }
+};
+</script>
---
+++ src/components/dev-menu-list.vue
@@ -0,0 +1,13 @@
+ <template
+      v-for="(node, index) in nodes" 
+      :key="index" 
+      :nodes="node"
+      icon="account">
+      <b-menu-item label="label"></b-menu-item>
+</template>
+<script>
+export default {
+  props: ["label", "nodes"],
+  name: 'dev-menu-list'
+};
+</script>
